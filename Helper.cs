e6e4b687f65b5eec1da3e6fed90f5832718defc7
Hycarus <mirkomarasco08@gmail.com>
2024-04-23 15:21:07 +0200

    exercise completed

---
+++ Helper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+namespace csharp_calcolatrice
+{
+	public static class Helper
+	{
+		public static int Somma(int n1, int n2)
+		{
+			return n1 + n2;
+		}
+
+		public static double Somma(double n1, double n2)
+		{
+			return n1 + n2;
+		}
+
+		public static int Differenza(int n1, int n2)
+		{
+			return n1 - n2;
+		}
+
+		public static double Differenza(double n1, double n2)
+		{
+			return n1 - n2;
+		}
+
+		public static int Moltiplicazione(int n1, int n2)
+		{
+			return n1 * n2;
+		}
+
+		public static double Moltiplicazione(double n1, double n2)
+		{
+			return n1 * n2;
+		}
+
+		public static int ValoreAssoluto(int num)
+		{
+			if (num > 0)
+				return num;
+			else
+				return -num;
+		}
+
+		public static double ValoreAssoluto(double num)
+		{
+			if (num > 0)
+				return num;
+			else
+				return -num;
+		}
+
+		public static int Minimo(int n1, int n2)
+		{
+			if (n1 > n2)
+				return n2;
+			else
+				return n1;
+		}
+
+		public static double Minimo(double n1, double n2)
+		{
+			if (n1 > n2)
+				return n2;
+			else
+				return n1;
+		}
+
+		public static int Massimo(int n1, int n2)
+		{
+			if (n1 > n2)
+				return n1;
+			else
+				return n2;
+		}
+
+		public static double Massimo(double n1, double n2)
+		{
+			if (n1 > n2)
+				return n1;
+			else
+				return n2;
+		}
+
+		public static double Potenza(int nBase, int nEsponente)
+		{
+			if (nBase == 0 && nEsponente == 0)
+				return 1;
+			if (nBase == 0)
+				return 0;
+			if (nEsponente == 0)
+				return 1;
+
+			double risultato = 1.0;
+            bool isNegativeExponent = nEsponente < 0;
+            if (isNegativeExponent)
+            {
+                nEsponente = -nEsponente;
+            }
+            for (int i = 0; i < nEsponente; i++)
+			{
+				risultato *= nBase;
+			}
+            if (isNegativeExponent)
+            {
+                risultato = 1.0 / risultato;
+            }
+            return risultato;
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace csharp_calcolatrice;
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace csharp_calcolatrice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"Somma di due interi: {Helper.Somma(5, 6)}");
+        Console.WriteLine($"Somma di due double: {Helper.Somma(5.5, 6.2)}");
+        Console.WriteLine($"Differenza di due interi: {Helper.Differenza(4, 6)}");
+        Console.WriteLine($"Differenza di due double: {Helper.Differenza(2.5, 6.8)}");
+        Console.WriteLine($"Moltiplicazione di due interi: {Helper.Moltiplicazione(8, 12)}");
+        Console.WriteLine($"Moltiplicazione di due double: {Helper.Moltiplicazione(2.8, 15.4)}");
+        Console.WriteLine($"Valore assoluto di un intero: {Helper.ValoreAssoluto(-5)}");
+        Console.WriteLine($"Valore assoluto di un double: {Helper.ValoreAssoluto(-5.5)}");
+        Console.WriteLine($"Minimo tra due interi: {Helper.Minimo(5, 9)}");
+        Console.WriteLine($"Minimo tra due double: {Helper.Minimo(5.3, 9.2)}");
+        Console.WriteLine($"Massimo tra due interi: {Helper.Massimo(5, 9)}");
+        Console.WriteLine($"Massimo tra due double: {Helper.Massimo(5.8, 9.4)}");
+
+        //Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto oggi…Quale ?
+        // IL POLIMORFISMO
+
+        //Bonus
+        Console.WriteLine($"Elevazione a potenza: {Helper.Potenza(0, 1)}");
+
     }
 }
 
